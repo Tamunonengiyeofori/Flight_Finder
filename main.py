53d248b08280af9ea04e98dcfb1eabc13784d3b9
Tamunonengiyeofori <tkennwariso@gmail.com>
2022-02-11 22:59:56 +0100

    These are the updates to The DataManager class and the FlightSearch class

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+# import pretty print => pprint to format json
+from pprint import pprint
+data_manager = DataManager()
+
+sheet_data = data_manager.get_all_data()
